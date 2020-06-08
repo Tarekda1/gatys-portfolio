b7fb74c4e9f9b369ed80b8cdc59674751ef38175
Tarekda1 <tare2.da@gmail.com>
2020-06-08 10:26:15 -0400

    all

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -4,8 +4,39 @@ import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
+const query = graphql`
+  {
+    allJobs: allStrapiJobs {
+      jobs: nodes {
+        id
+        date
+        position
+        company
+        desc {
+          job_description
+        }
+      }
+    }
+  }
+`
+
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  const {
+    allJobs: { jobs },
+  } = useStaticQuery(query)
+  return (
+    <section className="section jobs">
+      <Title title="Experience" />
+      <div className="jobs-center">
+        {/*btn containter */}
+        {jobs.map(job => {
+          const { id, date, position, company, desc } = job
+          return <article key={id}>{company}</article>
+        })}
+        {/*job info */}
+      </div>
+    </section>
+  )
 }
 
 export default Jobs
